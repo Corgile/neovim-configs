c9db1326c2e34c3cf8a56e1dbc9537596b1fd2b8
Corgile <mashiro-e@qq.com>
2022-11-16 02:33:51 +0800

    map home/end

--- lua/jay/core/keymap.lua
+++ lua/jay/core/keymap.lua
@@ -10,10 +10,10 @@ keymap.set("i", "jk", "<ESC>")
 keymap.set("n", "x", '"_x')             -- 删除而不是剪切
 
 ----- split window
-keymap.set("n", "<leader>sv", "<C-w>v")     -- split vertically
-keymap.set("n", "<leader>sh", "<C-w>s")     -- split horizontally
-keymap.set("n", "<leader>se", "<C-w>=")     -- adjust split windows to the same height/width
-keymap.set("n", "<leader>sc", ":close<CR>") -- close current split window
+keymap.set("n", "<LEADER>sv", "<C-w>v")     -- split vertically
+keymap.set("n", "<LEADER>sh", "<C-w>s")     -- split horizontally
+keymap.set("n", "<LEADER>se", "<C-w>=")     -- adjust split windows to the same height/width
+keymap.set("n", "<LEADER>sc", ":close<CR>") -- close current split window
 
 -- move between windows
 keymap.set("n", "<C-LEFT>", ":TmuxNavigateLeft<CR>")
@@ -24,13 +24,13 @@ keymap.set("n", "<C-DOWN>", ":TmuxNavigateDown<CR>")
 -- tab management, using bufferline.nvim
 keymap.set("n", "<M-RIGHT>", ":BufferLineCycleNext<CR>")    -- select next tab
 keymap.set("n", "<M-LEFT>",  ":BufferLineCyclePrev<CR>")    -- select previous tab
-keymap.set("n", "<leader>b", ":BufferLinePick<CR>")         -- pick up a tab
-keymap.set("n", "<leader>x", ":BufferLinePickClose<CR>")    -- pick up a tab to close
+keymap.set("n", "<LEADER>b", ":BufferLinePick<CR>")         -- pick up a tab
+keymap.set("n", "<LEADER>x", ":BufferLinePickClose<CR>")    -- pick up a tab to close
 
 -------- Native mappings
 -- home / end
-keymap.set("n", "<S-LEFT>", "^")
-keymap.set("n", "<S-RIGHT>", "$")
+keymap.set("n", "<LEADER><LEFT>", "^")
+keymap.set("n", "<LEADER><RIGHT>", "$")
 
 -- folding
 keymap.set("v", "<BS>", "zf")   -- fold selected lines
@@ -45,17 +45,17 @@ keymap.set("n", "W", "B")
 ------ Plugin keymaps
 
 -- maximize and restore split window
-keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>")
+keymap.set("n", "<LEADER>sm", ":MaximizerToggle<CR>")
 -- nvim-tree
-keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
-keymap.set("n", "<leader>m", ":NvimTreeFocus<CR>")
+keymap.set("n", "<LEADER>e", ":NvimTreeToggle<CR>")
+keymap.set("n", "<LEADER>m", ":NvimTreeFocus<CR>")
 -- keymap.set("n", "<Left>", ":NvimTreeCollapes")
 -- keymap.set("n", "<Right>", ":NvimTreeOpen")
 -- fuzzy finder / telescope
-keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>")
-keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<CR>")
-keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<CR>")
-keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<CR>")
-keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<CR>")
+keymap.set("n", "<LEADER>ff", "<cmd>Telescope find_files<CR>")
+keymap.set("n", "<LEADER>fs", "<cmd>Telescope live_grep<CR>")
+keymap.set("n", "<LEADER>fc", "<cmd>Telescope grep_string<CR>")
+keymap.set("n", "<LEADER>fb", "<cmd>Telescope buffers<CR>")
+keymap.set("n", "<LEADER>fh", "<cmd>Telescope help_tags<CR>")
 -- restart lsp server (not on youtube nvim video)
-keymap.set("n", "<leader>rs", ":LspRestart<CR>") -- mapping to restart lsp if necessary
+keymap.set("n", "<LEADER>rs", ":LspRestart<CR>") -- mapping to restart lsp if necessary
