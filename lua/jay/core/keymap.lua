2631fec56baa77548ac629ff551c1401c0d0a880
Corgile <mashiro-e@qq.com>
2022-11-16 16:25:16 +0800

    keymap changes on macOS

--- lua/jay/core/keymap.lua
+++ lua/jay/core/keymap.lua
@@ -8,25 +8,29 @@ keymap.set("i", "jk", "<ESC>")
 
 -- normal mode
 keymap.set("n", "x", '"_x')             -- 删除而不是剪切
-keymap.set("n", "<leader>+", "<C-a>")   -- 增加数值
-keymap.set("n", "<leader>-", "<C-x>")   -- 减小数值
+keymap.set("n", "<LEADER>+", "<C-a>")   -- 增加数值
+keymap.set("n", "<LEADER>-", "<C-x>")   -- 减小数值
 
 ----- split window
-keymap.set("n", "<leader>sv", "<C-w>v")     -- split vertically
-keymap.set("n", "<leader>sh", "<C-w>s")     -- split horizontally
-keymap.set("n", "<leader>se", "<C-w>=")     -- adjust split windows to the same height/width
-keymap.set("n", "<leader>sc", ":close<CR>") -- close current split window
+keymap.set("n", "<LEADER>sv", "<C-w>v")     -- split vertically
+keymap.set("n", "<LEADER>sh", "<C-w>s")     -- split horizontally
+keymap.set("n", "<LEADER>se", "<C-w>=")     -- adjust split windows to the same height/width
+keymap.set("n", "<LEADER>sc", ":close<CR>") -- close current split window
+keymap.set("n", "<C-LEFT>",  ":TmuxNavigateLeft<CR>")
+keymap.set("n", "<C-RIGHT>", ":TmuxNavigateRight<CR>")
+keymap.set("n", "<C-UP>",    ":TmuxNavigateUp<CR>")
+keymap.set("n", "<C-DOWN>",  ":TmuxNavigateDown<CR>")
 
 -- tab management
-keymap.set("n", "<leader>tt", ":tabnew<CR>")   -- new tab
-keymap.set("n", "<leader>x", ":tabclose<CR>")  -- close current tab
-keymap.set("n", "<leader>n", ":tabn<CR>")    -- next tab
-keymap.set("n", "<leader>m", ":tabp<CR>")    -- previous tab
+keymap.set("n", "<M-LEFT>",  ":BufferLineCyclePrev<CR>")  -- close current tab
+keymap.set("n", "<M-RIGHT>", ":BufferLineCycleNext<CR>")    -- next tab
+keymap.set("n", "<LEADER>b", ":BufferLinePick<CR>")    -- previous tab
+keymap.set("n", "<LEADER>x", ":BufferLinePickClose<CR>")    -- previous tab
 
 -------- Native mappings
 -- home / end
-keymap.set("n", "<M-LEFT>", "^")
-keymap.set("n", "<M-RIGHT>", "$")
+keymap.set("n", "<S-LEFT>", "^")
+keymap.set("n", "<S-RIGHT>", "$")
 
 -- folding
 keymap.set("v", "<BS>", "zf")   -- fold selected lines
@@ -41,16 +45,17 @@ keymap.set("n", "W", "B")
 ------ Plugin keymaps
 
 -- maximize and restore split window
-keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>")
+keymap.set("n", "<LEADER>sm", ":MaximizerToggle<CR>")
 -- nvim-tree
-keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
+keymap.set("n", "<LEADER>e", ":NvimTreeToggle<CR>")
+keymap.set("n", "<LEADER>m", ":NvimTreeFocus<CR>")
 -- keymap.set("n", "<Left>", ":NvimTreeCollapes")
 -- keymap.set("n", "<Right>", ":NvimTreeOpen")
 -- fuzzy finder / telescope
-keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>")
-keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<CR>")
-keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<CR>")
-keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<CR>")
-keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<CR>")
+keymap.set("n", "<LEADER>ff", "<cmd>Telescope find_files<CR>")
+keymap.set("n", "<LEADER>fs", "<cmd>Telescope live_grep<CR>")
+keymap.set("n", "<LEADER>fc", "<cmd>Telescope grep_string<CR>")
+keymap.set("n", "<LEADER>fb", "<cmd>Telescope buffers<CR>")
+keymap.set("n", "<LEADER>fh", "<cmd>Telescope help_tags<CR>")
 -- restart lsp server (not on youtube nvim video)
-keymap.set("n", "<leader>rs", ":LspRestart<CR>") -- mapping to restart lsp if necessary
+keymap.set("n", "<LEADER>rs", ":LspRestart<CR>") -- mapping to restart lsp if necessary
